3b1a7a594c3f62ada6a72bba49b47b25faa36f68
Valkryn <calvinraphaelg@gmail.com>
2020-12-19 15:17:46 -0500

    back end finished/ created -react-app-client

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,22 @@
-const jwt = require("jsonwebtoken")
-const config = require("config")
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
+module.exports = function(req, res, next) {
+	// Get token from header
+	const token = req.header('x-auth-token');
 
+	// Check if not token
+	if (!token) {
+		return res.status(401).json({ msg: 'No token, authorization denied' });
+	}
 
+	// Verify token
+	try {
+		const decoded = jwt.verify(token, config.get('jwtSecret'));
 
-module.exports = function(req, res, next){
-  // get token from header
-  const token = req.header("x-auth-token")
-
-  //check if the token exists
-  if(!token) {
-    return res.status(401).json({msg:"No token, authorization denied"})
-  }
-
-  try{
-    const decoded = jwt.verify(token,config.get("jwtsecret"))
-    req.user = decoded.user
-    next()
-  } catch(err) {
-    res.status(401).json({msg:"token not valid"})
-  }
-
-}
+		req.user = decoded.user;
+		next();
+	} catch (err) {
+		res.status(401).json({ msg: 'Token is not valid' });
+	}
+};
